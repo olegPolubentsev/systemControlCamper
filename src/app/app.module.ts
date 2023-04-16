3dc1d45f5b6dd69184a940f08d4d1c9a5520922b
olegPolubentsev <79081696+olegPolubentsev@users.noreply.github.com>
2023-04-16 12:10:04 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { TopBlockComponent } from './top-block/top-block.component';
+import {SharedModule} from "./shared/shared.module";
+import { LoginComponent } from './content-block/login/login.component';
+import { HomeComponent } from './content-block/home/home.component';
+import { CamperComponent } from './content-block/camper/camper.component';
+import { ErrPageComponent } from './content-block/err-page/err-page.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { AboutComponent } from './content-block/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBlockComponent,
+    LoginComponent,
+    HomeComponent,
+    CamperComponent,
+    ErrPageComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
